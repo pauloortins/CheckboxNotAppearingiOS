e9332effa92aabe0129452536082d650c6e23365
pauloortins <paulo.ortins@gmail.com>
2015-03-09 10:55:04 -0300

    Commit Zero

---
+++ CheckboxTesting/CheckboxTesting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Xamarin.Forms;
+using XLabs.Forms.Controls;
+
+namespace CheckboxTesting
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new StackLayout() 
+						{
+							BackgroundColor = Color.White,
+							Children = 
+							{ 
+								new CheckBox() 
+								{
+									CheckedText = "Checked",
+									UncheckedText = "Unchecked"
+								}
+							}
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
